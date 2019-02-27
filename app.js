0f3744395c36c69095f9121c66ddd1ac2b3539e9
afmire877 <33981371+afmire877@users.noreply.github.com>
2019-02-27 07:31:02 +0000

    Deleted Extra Image

---
+++ app.js
@@ -0,0 +1,21 @@
+var answer = document.querySelectorAll(".answer");
+var questions = document.querySelectorAll(".question");
+
+
+
+for(var i= 0; i <= questions.length -1 ;i++){
+  questions[i].addEventListener("click", function(e){
+    var ans = e.toElement.lastElementChild;
+    ans.classList.toggle("displayAnswer");
+
+  });
+  }
+
+  // function showAnswer(e){
+  //   if (e.target !== e.currentTarget ){
+  //     var clickedElement = e.target.class;
+  //     cosole.log(asnwer[i])
+
+  //   }
+  //   e.stopPropagation();
+  // };
